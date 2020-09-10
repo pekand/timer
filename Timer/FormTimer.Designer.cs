231dc90f7c3d55b85ced883c13d6628d0ba5104e
Andrej Pekar <pekand@gmail.com>
2020-09-10 18:29:40 +0200

    init

---
+++ Timer/FormTimer.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Timer
+{
+    partial class FormTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTimer));
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.ContextMenuStrip = this.contextMenuStrip;
+            this.labelTimer.Font = new System.Drawing.Font("Consolas", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimer.Location = new System.Drawing.Point(12, 9);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(0, 101);
+            this.labelTimer.TabIndex = 0;
+            this.labelTimer.MouseClick += new System.Windows.Forms.MouseEventHandler(this.labelTimer_MouseClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pauseToolStripMenuItem,
+            this.resetToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip1";
+            this.contextMenuStrip.Size = new System.Drawing.Size(181, 70);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resetToolStripMenuItem.Text = "reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pauseToolStripMenuItem.Text = "pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.pauseToolStripMenuItem_Click);
+            // 
+            // FormTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 123);
+            this.ContextMenuStrip = this.contextMenuStrip;
+            this.Controls.Add(this.labelTimer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormTimer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormTimer_FormClosing);
+            this.Load += new System.EventHandler(this.FormTimer_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FormTimer_MouseClick);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+    }
+}
+
---
+++ Timer/FormTimer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class FormTimer : Form
+    {
+        private long cTimer = 0;
+        private long aTimer = 0;
+        private bool paused = false;
+
+        public FormTimer()
+        {
+            InitializeComponent(); 
+        }
+
+        private void FormTimer_Load(object sender, EventArgs e)
+        {
+            cTimer = Properties.Settings.Default.cTime;
+            aTimer = new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds();
+
+            this.timer_Tick(this, null);
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            refreshLabel();
+        }
+
+        private void refreshLabel() {
+            labelTimer.Text = formatSeconds(cTimer + countTimerDiff());
+        }
+
+
+        private long countTimerDiff()
+        {
+            var timeNow = new DateTimeOffset(DateTime.Now);
+            return (timeNow.ToUnixTimeMilliseconds() - aTimer);
+        }
+
+        private string formatSeconds(long totalSeconds = 0) {
+            long miliseconds = totalSeconds % 1000;
+            totalSeconds = totalSeconds / 1000;
+            
+            long seconds = totalSeconds % 60;
+            totalSeconds = totalSeconds / 60;
+
+            long minutes = totalSeconds % 60;
+            totalSeconds = totalSeconds / 60;
+
+            long hours = totalSeconds % 60;
+            totalSeconds = totalSeconds / 60;
+
+            return padInt(hours, 2) + ":" + padInt(minutes, 2) + ":" + padInt(seconds, 2);
+        }
+
+        private string padInt(long i, int pad) { 
+            return i.ToString().PadLeft(pad, '0');
+        }
+
+        private void FormTimer_MouseClick(object sender, MouseEventArgs e)
+        {
+            pauseTimer();
+        }
+
+        private void labelTimer_MouseClick(object sender, MouseEventArgs e)
+        {
+            pauseTimer();
+        }
+
+        private void pauseTimer() {
+            if (paused)
+            {
+                this.Text = "Timer";
+                aTimer = new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds();
+                timer.Enabled = true;
+                paused = false;
+            }
+            else
+            {
+                this.Text = "Timer - Paused";
+                cTimer += countTimerDiff();
+                timer.Enabled = false;
+                paused = true;
+            }
+        }
+
+        private void FormTimer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.cTime = cTimer + countTimerDiff();
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void resetTimer() {
+            aTimer = new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds();
+            cTimer = 0;
+            Properties.Settings.Default.cTime = 0;
+            Properties.Settings.Default.Save();
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            resetTimer();
+            refreshLabel();
+        }
+
+        private void pauseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pauseTimer();
+        }
+    }
+}
